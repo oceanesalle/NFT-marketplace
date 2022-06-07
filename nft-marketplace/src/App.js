dd4f1ad698e5db2b118154d44547c9e2f50ebe98
oceansalle <oceanesalleiae@gmail.com>
2022-06-07 11:31:25 +0200

    Navbar & button components

--- nft-marketplace/src/App.js
+++ nft-marketplace/src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      App
+      <Home />
+      <Navbar />
     </div>
   );
 }
