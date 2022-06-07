b06536a3cb474a1265b1e372cf1a37d53f84f86f
oceansalle <oceanesalleiae@gmail.com>
2022-06-07 15:53:20 +0200

    client component

--- nft-marketplace/src/App.js
+++ nft-marketplace/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Clients from "./components/Clients";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Navbar />
+      <Clients />
+
     </div>
   );
 }
